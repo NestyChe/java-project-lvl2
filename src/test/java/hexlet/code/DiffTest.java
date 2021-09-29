58a3f017e6d851829278c06c52db70e7a5cb3fad
Your Name <you@example.com>
2021-09-29 15:53:30 +0400

    change test

--- src/test/java/hexlet/code/DiffTest.java
+++ src/test/java/hexlet/code/DiffTest.java
@@ -1,17 +1,30 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DiffTest {
 
     @Test
-    public  void generateTest() throws IOException {
+    public String expected(String name) throws IOException {
+        return Files.newBufferedReader(Paths.get(name))
+                .lines()
+                .collect(Collectors.joining());
+    }
+
+    @Test
+    public  void generateDiffTest() throws IOException {
         String first = "/home/main/java-project-lvl2/src/test/resources/first.json";
-        String second = "/home/main/java-project-lvl2/src/test/resources/second.json";
-        Differ.generate(first, second);
+        String second= "/home/main/java-project-lvl2/src/test/resources/second.json";
+        String pathExpectFile = "/home/main/java-project-lvl2/src/test/resources/expectedFile/json_diff";
+        assertEquals(expected(pathExpectFile), Differ.generate(first, second));
     }
 }
