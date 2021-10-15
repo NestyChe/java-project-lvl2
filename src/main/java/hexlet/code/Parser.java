3f9c521ab88846f257e14c9f1325c9784ac29bbc
Your Name <you@example.com>
2021-10-15 11:27:26 +0400

    add parser class

---
+++ src/main/java/hexlet/code/Parser.java
@@ -0,0 +1,28 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class Parser {
+
+    public static ObjectMapper parse(String type) {
+        switch (type.toLowerCase()) {
+            case "json":
+                return new ObjectMapper();
+            case "yaml":
+                return new ObjectMapper(new YAMLFactory());
+            default:
+                throw new IllegalStateException("Unexpected type " + type);
+        }
+    }
+
+    public static Map<String, Object> fileToMap(String type, String file) throws IOException {
+        return parse(type).readValue(file, new TypeReference<>() { });
+    }
+
+    //public static Path getPath(String filePath) {return Paths.get(filePath).toAbsolutePath();}
+}
