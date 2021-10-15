5d50e2c0f17a8bd6f372da6eaf297e3811215514
Your Name <you@example.com>
2021-10-15 11:32:58 +0400

    rewrite test

--- src/test/java/hexlet/code/DiffTest.java
+++ src/test/java/hexlet/code/DiffTest.java
@@ -2,29 +2,41 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 import java.io.*;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DiffTest {
 
     @Test
-    public  void generateDiffTest() throws IOException {
-        String first = "/home/main/java-project-lvl2/src/test/resources/first.json";
-        String second = "/home/main/java-project-lvl2/src/test/resources/second.json";
-        String pathExpectFile = "/home/main/java-project-lvl2/src/test/resources/expectedFile/json_diff";
-        assertEquals(expected(pathExpectFile), Differ.generate(first, second));
+    public void generateDiffTest() throws IOException {
+        for (String[] d : files()) {
+            assertEquals(expected(d[2]), Differ.generate(d[0], d[1],"stylish"));
+        }
     }
 
     public String expected(String name) throws IOException {
-        return Files.newBufferedReader(Paths.get(name))
+        String path = "/home/main/java-project-lvl2/src/test/resources";
+        return Files.newBufferedReader(Paths.get(path + name))
                 .lines()
                 .collect(Collectors.joining());
     }
 
-    /*public Stream<String> files() {
-
-    }*/
+    public List<String[]> files() {
+        return Arrays.asList(
+                new String[]{
+                    "first.json",
+                    "second.json",
+                    "/expectedfile/json_diff"},
+                new String[]{
+                    "first.yaml",
+                    "second.yaml",
+                    "/expectedfile/yaml_diff"}
+        );
+    }
 }
