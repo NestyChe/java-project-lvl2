2d79fe4f1be4975c26adc700f2387713d4affac6
Your Name <you@example.com>
2021-12-10 17:33:09 +0400

    fix test

--- src/test/java/hexlet/code/DiffTest.java
+++ src/test/java/hexlet/code/DiffTest.java
@@ -2,7 +2,6 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 import java.io.*;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -14,29 +13,60 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class DiffTest {
 
     @Test
-    public void generateDiffTest() throws IOException {
-        for (String[] d : files()) {
-            assertEquals(expected(d[2]), Differ.generate(d[0], d[1],"stylish"));
+    public void stylishFormatterTest() throws IOException {
+        generateDiffTest("stylish");
+    }
+
+    @Test
+    public void plainFormatterTest() throws IOException {
+        generateDiffTest("plain");
+    }
+
+    @Test
+    public void  jsonFormatterTest() throws IOException {
+        generateDiffTest("json");
+    }
+
+    public void generateDiffTest(String formatTest) throws IOException {
+        String expectSimple = expected(formatTest + "_simple");
+        String expectNested = expected(formatTest + "_nested");
+        for (String[] file : files()) {
+            String first = getFile(file[0]);
+            String second = getFile(file[1]);
+            if (file[0].startsWith("simple")) {
+                assertEquals(expectSimple, Differ.generate(first, second, formatTest));
+            } else {
+                assertEquals(expectNested,  Differ.generate(first, second, formatTest));
+            }
         }
     }
 
+    public static String getFile(String name) {
+        File f = new File(Differ.class.getClassLoader().getResource(name).getFile());
+        return f.getPath();
+    }
+
     public String expected(String name) throws IOException {
-        String path = "/home/main/java-project-lvl2/src/test/resources";
+        String path = "/home/main/Nastya/my-projects/java-project-lvl2/src/test/resources/expectedfile/";
         return Files.newBufferedReader(Paths.get(path + name))
                 .lines()
-                .collect(Collectors.joining());
+                .collect(Collectors.joining("\n"));
     }
 
     public List<String[]> files() {
         return Arrays.asList(
                 new String[]{
-                    "first.json",
-                    "second.json",
-                    "/expectedfile/json_diff"},
+                    "simple_first.json",
+                    "simple_second.json"},
                 new String[]{
-                    "first.yaml",
-                    "second.yaml",
-                    "/expectedfile/yaml_diff"}
+                    "simple_first.yaml",
+                    "simple_second.yaml"},
+                new String[]{
+                    "nested_first.json",
+                    "nested_second.json"},
+                new String[]{
+                    "nested_first.yaml",
+                    "nested_second.yaml"}
         );
     }
 }
