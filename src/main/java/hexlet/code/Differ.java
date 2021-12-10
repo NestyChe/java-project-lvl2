b011fde793dcaf28dec44b219e4f85ea7e021b40
Your Name <you@example.com>
2021-12-10 17:37:01 +0400

    rewrite methods

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -1,46 +1,45 @@
 package hexlet.code;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import static hexlet.code.Status.*;
+import static java.util.Comparator.comparing;
+import static java.util.stream.Stream.concat;
 
 public class Differ {
 
     public static String generate(String filePath1, String filePath2, String formatName) throws IOException {
         Map<String, Object> first = getContent(filePath1);
         Map<String, Object> second = getContent(filePath2);
-        return Formatter.formatter(first, second, formatName);
+
+        return concat(first.keySet().stream(), second.keySet().stream())
+                .distinct()
+                .map(pointName -> new Point(pointName,
+                                first.get(pointName),
+                                second.get(pointName),
+                                getStatus(first, second, pointName)
+                        )
+                ).sorted(comparing(Point::getKey))
+                .collect(Formatter.formatter(formatName));
+
     }
 
 
     public static Map<String, Object> getContent(String path) throws IOException {
-        String file = Files.readString(getFileByName(path)); //Paths.get().toAbsolutePath()
+        String file = Files.readString(Paths.get(path));
         String type = path.substring(path.indexOf('.') + 1);
         return Parser.fileToMap(type, file);
     }
 
-    //???????
-    public static Path getFileByName(String name) {
-        File f = new File(Differ.class.getClassLoader().getResource(name).getFile());
-        return f.toPath();
-        /*ClassLoader classLoader = getClass().getClassLoader();
-        URL resource = classLoader.getResource(name);
-        return new File(resource.toURI()).toPath();*/
+
+    public static Status getStatus(final Map<String, Object> content1,
+                                   final Map<String, Object> content2,
+                                   final String pointName) {
+        return !content1.containsKey(pointName) ? ADDED
+                : !content2.containsKey(pointName) ? REMOVED
+                : Objects.equals(content1.get(pointName), content2.get(pointName)) ? UNCHANGED
+                : CHANGED;
     }
 }
-
-
-
-/*public class Differ {
-    public static String generate(String filePath1, String filePath2) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper(new JsonFactory());
-        JsonNode file1 = objectMapper.readTree(new File(filePath1));
-        JsonNode file2 = objectMapper.readTree(new File(filePath2));
-        JsonNode patch = JsonDiff.asJson(file1, file2);
-        String diffs = patch.toString();
-        System.out.println(diffs);
-        return diffs;
-    }
-}*/
